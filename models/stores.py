ad337d845f0aedeeacf9dc05545db92b9182dc76
Falaye Promise <promiseanuoluwa@gmail.com>
2023-05-01 13:35:26 +0100

    Add cascade to store model

--- models/stores.py
+++ models/stores.py
@@ -4,6 +4,6 @@ class StoreModel(db.Model):
     __tablename__ = 'stores'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
-    items = db.relationship('ItemModel', back_populates='store', lazy='dynamic') # this returns a store model object related with the item.
+    items = db.relationship('ItemModel', back_populates='store', lazy='dynamic', cascade='all, delete') # this returns a store model object related with the item.
     tags = db.relationship('TagModel', back_populates='store', lazy='dynamic')
     
